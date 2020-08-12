1665b2510df583b11762974883d81c00094e4dd3
Aniket Kumar Sinha <sinhaaniket32@gmail.com>
2020-08-13 00:21:20 +0530

    github-account

---
+++ src/Config/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+    apiKey: "AIzaSyAK95PJkFfg4jtx9TU1zBnhCVdZHevsmFE",
+    authDomain: "github-account.firebaseapp.com",
+    databaseURL: "https://github-account.firebaseio.com",
+    projectId: "github-account",
+    storageBucket: "github-account.appspot.com",
+    messagingSenderId: "870155848437",
+    appId: "1:870155848437:web:24336546eabfc2783fd3bc"
+  };
---
+++ src/layout/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Container} from 'reactstrap';
+
+const Footer = () => {
+    return (
+        <Container fluid
+            tag="footer"
+            className="text-center bg-info text-white fixed-bottom p-3"
+        >Get-o-GitHub | Made with ❤️ by Aniket Kumar Sinha
+        </Container>
+    )
+}
+
+export default Footer;
