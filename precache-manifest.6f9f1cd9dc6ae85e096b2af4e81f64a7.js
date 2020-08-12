0dd8f0a1c0ca636aafa7511f7cb82106ddbf85f5
Aniket Kumar Sinha <sinhaaniket32@gmail.com>
2020-08-13 00:20:13 +0530

    Updates

---
+++ precache-manifest.6f9f1cd9dc6ae85e096b2af4e81f64a7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d319646c43b71bc7eb5a47e814cef254",
+    "url": "/git-o-github/index.html"
+  },
+  {
+    "revision": "6e0a80babe73faeb84e8",
+    "url": "/git-o-github/static/css/2.36155cc5.chunk.css"
+  },
+  {
+    "revision": "616a0d62606a7a71ae07",
+    "url": "/git-o-github/static/css/main.70192d26.chunk.css"
+  },
+  {
+    "revision": "6e0a80babe73faeb84e8",
+    "url": "/git-o-github/static/js/2.c31bc6ac.chunk.js"
+  },
+  {
+    "revision": "df2659f20723d9e518ad965c595b69ee",
+    "url": "/git-o-github/static/js/2.c31bc6ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "616a0d62606a7a71ae07",
+    "url": "/git-o-github/static/js/main.0734fa66.chunk.js"
+  },
+  {
+    "revision": "fd08a6ae45e4839b4385",
+    "url": "/git-o-github/static/js/runtime-main.9e85aaf6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/git-o-github/static/media/logo.5d5d9eef.svg"
+  }
+]);
